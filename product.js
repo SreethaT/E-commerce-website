f9f724ca159a5b4b929add0b5a861a1bf0cb2f8a
SreethaT <99554371+SreethaT@users.noreply.github.com>
2022-03-22 22:10:41 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,10 @@
+
+const sizeBtns = document.querySelectorAll('.size-radio-btn'); 
+let checkedBtn = 0;
+sizeBtns.forEach((item, i) => { 
+    item.addEventListener('click', () => {
+        sizeBtns[checkedBtn].classList.remove('check'); 
+        item.classList.add('check'); 
+        checkedBtn = i; 
+    })
+})
